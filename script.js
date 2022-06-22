309a51ed05fe48b27fe2c162a3f3349ae41d3c90
Sean Genge <seangenge@gmail.com>
2022-06-22 12:13:11 +1000

    Added form to modal and fixed instructions

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+projectTypes = ["Web Application (Full stack)", "Web Application (Back-end)", "Web Application (front-end)", "Application programming interface", "Database"]
 
 function displayCurrentTime(){
     $("#current-time").text(moment().format("MMM D, YYYY [at] h:mm:ss a"));
 }
 
+function initProjectTypes() {
+    // Dynamically add the project types to the select element
+    for (var i = 0;  i < projectTypes.length; i++) {
+        var option = document.createElement("option");
+        var select = document.getElementById("projectType");
+        
+        option.value = projectTypes[i];
+        option.textContent = projectTypes[i];
+        
+        select.appendChild(option);
+    }
+}
+
 setInterval(displayCurrentTime);
-
-$('#modal-button').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
-
-
-
+initProjectTypes();
 
 
